3e29427dceb19752719144f1dc4d566abcfca1c9
Yegor Zhumikov <yegor@yogurtthehor.se>
2022-03-25 05:10:27 +0300

    feat: reload for shooting

--- Assets/Scripts/Rockets/RocketLauncher.cs
+++ Assets/Scripts/Rockets/RocketLauncher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,17 @@ using Zenject;
 
 public class RocketLauncher : MonoBehaviour
 {
+    public float reloadTime = 0.3f;
+
+    public float reloadLeft = 0;
+
     public Transform[] launchTransforms;
 
     public int launchIndex;
-    
+
     [Inject]
     private RocketPool _rocketPool;
-    
+
     public void Start()
     {
         if (launchTransforms == null || launchTransforms.Length == 0)
@@ -20,8 +25,21 @@ public class RocketLauncher : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        if (reloadLeft > 0)
+        {
+            reloadLeft -= Time.deltaTime;
+        }
+    }
+
     public void Launch()
     {
+        if (reloadLeft > 0)
+            return;
+
+        reloadLeft = reloadTime;
+        
         launchIndex = (launchIndex + 1) % launchTransforms.Length;
         var t = launchTransforms[launchIndex];
 
